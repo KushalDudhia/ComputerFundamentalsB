693b47f6170bb67036b151c0108844ef691cba4f
KushalDudhia <dudhiakushal@gmail.com>
2024-03-08 14:25:38 +0530

    jhbjh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,7 @@
-git branch branch2
-git checkout branch2
+git checkout -b branch2
 touch file4
 git add file4
 git commit -m "A4"
 echo "New file4" > file4
+git stash
 git checkout main
---
+++ question12.sh
@@ -0,0 +1,6 @@
+git checkout branch2
+git stash pop
+git add .
+git commit -m "kjhuh"
+
+
