c76624dbf8f05aecdabf76d4aeb7678e3dbed29e
KushalDudhia <dudhiakushal@gmail.com>
2024-03-09 06:04:09 +0530

    la

--- question15.sh
+++ question15.sh
@@ -1,6 +1,6 @@
-ggit clone https://github.com/ian-knight-uofa/git-practice-01.git question15
-cd question15/
+ggit clone https://github.com/ian-knight-uofa/git-practice-01.git 
 git fetch origin
 git checkout branch1
 git checkout main
 git merge branch1
+
